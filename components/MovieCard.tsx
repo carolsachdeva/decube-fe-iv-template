352015cafc559116c59164f2aedd221104434ead
Aqmal <50930584+kifasu@users.noreply.github.com>
2024-04-01 14:10:22 +0800

    (style) movie card

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -40,27 +40,32 @@ const MovieCard = (props: MovieCardProps) => {
     return (
         <Link
             href={`/movie/${id}`}
-            className='bg-white rounded-md min-h-[300px] p-4 relative flex flex-col gap-3 cursor-pointer hover:opacity-50 transition-all opacity-100 group overflow-hidden'
+            className='bg-white rounded-md min-h-[300px] p-4 relative flex flex-col gap-3 cursor-pointer hover:opacity-30 transition-all opacity-100 group overflow-hidden'
         >
             <div
                 className={cn(
-                    'group-hover:top-0 group-hover:right-0 group-hover:left-0 group-hover:bottom-0',
-                    '-z-10 group-hover:z-10 z',
-                    'bg-black',
-                    'group-hover:h-full group-hover:w-full',
                     'absolute',
+                    'top-0 left-0 right-0 bottom-0',
+                    'group-hover:top-0 group-hover:right-0 group-hover:left-0 group-hover:bottom-0',
+                    '-z-10 group-hover:z-10',
+                    'group-hover:h-full group-hover:w-full',
                     'opacity-0 group-hover:opacity-100',
-                    'transition-all p-4 duration-500'
+                    'transition-all p-4 duration-500',
                 )}
                 style={{
-                    backgroundImage: `url(${process.env.NEXT_PUBLIC_API_IMAGE_PATH}/${backdrop_path}) no-repeat`,
+                    backgroundImage: `url('${process.env.NEXT_PUBLIC_API_IMAGE_PATH}/${backdrop_path}')`,
                     backgroundSize: 'cover',
                     backgroundPosition: 'center',
                     backgroundRepeat: 'no-repeat',
                 }}
             >
-                <div className='absolute right-4 '>
+                <div className='absolute right-4'>
                     <InfoIcon height={24} />
+                </div>
+                <div className='relative h-full w-full flex items-center justify-center'>
+                    <div className='py-2 px-3 border-gray-300 border-2 border-solid rounded-lg'>
+                        View Details
+                    </div>
                 </div>
             </div>
             {isNsfw()}
@@ -89,7 +94,7 @@ const MovieCard = (props: MovieCardProps) => {
                 <p className='text-sm text-gray-700 h-[100px] line-clamp-5 overflow-hidden'>
                     {overview}
                 </p>
-                <Link href={`/movie/${id}`} className='absolute bottom-0 right-0 text-gray-950 group-hover:font-semibold'>Read more</Link>
+                <div className='absolute bottom-0 right-0 text-gray-950 group-hover:font-semibold'>Read more</div>
             </div>
         </Link>
     )
